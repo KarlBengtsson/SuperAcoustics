9c11125e11b6919b2db4eb6b6054f07e8ffcfb56
elliotsandefeldt <elliotsandefeldt@gmail.com>
2019-02-15 11:38:02 +0100

    Implementation of sending preferences back & forth

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,27 +3,39 @@ package com.example.myapplication;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button dbMeter;
+    private TextView calTextView;
+    private static final String TAG = "MainActivity";
+    double mDifferenceFromNominal = 0.0;
+    int mAudioSource = 0;
+    int mSampleRate = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        readPreferences();
         dbMeter = (Button) findViewById(R.id.dbmeterButton);
-
+        calTextView = (TextView) findViewById(R.id.calibrateText);
+        calTextView.setText(Double.toString(mDifferenceFromNominal));
         onCheckPerm();
 
     }
+
 
     public void CalibrateSPL (View view) {
         Intent intent = new Intent(this, LevelMeterActivity.class);
@@ -53,5 +65,61 @@ public class MainActivity extends AppCompatActivity {
         if (!hasPermissions(this, PERMISSIONS)) {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         }
+    }
+    private void readPreferences() {
+        SharedPreferences preferences = getSharedPreferences("LevelMeter",
+                MODE_PRIVATE);
+        mSampleRate = preferences.getInt("SampleRate", 8000);
+        mAudioSource = preferences.getInt("AudioSource",
+                MediaRecorder.AudioSource.VOICE_RECOGNITION);
+        mDifferenceFromNominal = preferences.getInt("mGainDif", 0);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "onSaveInstanceState() called");
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "RestoreInstanceState() called");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    //Called when returning to Main Activity from Result Activity
+    protected void onResume() {
+        super.onResume();
+        readPreferences();
+        calTextView.setText(Double.toString(mDifferenceFromNominal));
+        Log.d(TAG, "onResume() called");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
